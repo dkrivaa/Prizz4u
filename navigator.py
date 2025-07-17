68b15cc7e36521b6f97f16d17e2bceccb84ec105
dkrivaa <dannykrivaa@hotmail.com>
2025-07-17 07:37:20 +0300

    Initial commit

--- navigator.py
+++ navigator.py
@@ -3,8 +3,8 @@ import streamlit as st
 
 def nav():
     """ This is the app navigation menu """
-    with st.container(border=True):
-        col1, col2, col3, col4 = st.columns([1, 1, 1, 2])
+    with st.container(border=False):
+        col1, col2, col3, col4 = st.columns([1, 1, 1, 4])
         with col1:
             st.page_link(page='views/store.py')
         with col2:
